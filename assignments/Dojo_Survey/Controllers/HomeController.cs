e1f4c58f925eab94cd9dbbaf98ad508546a26c61
MarkLambson <mj.lambson@yahoo.com>
2023-06-08 12:00:24 -0700

    Dojo Survey

--- assignments/Dojo_Survey/Controllers/HomeController.cs
+++ assignments/Dojo_Survey/Controllers/HomeController.cs
@@ -13,14 +13,41 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    [HttpGet("")]
     public IActionResult Index()
     {
         return View();
     }
+//work in here------------------------------------------------------------------------------------------//
 
+    [HttpPost("survey")]
+    public IActionResult SurveySave(string Name, string Location, string Language, string Comment)
+    {
+        ViewBag.Name = Name;
+        ViewBag.Location = Location;
+        ViewBag.Language = Language;
+        ViewBag.Comment = Comment;
+        // return view to not lose ViewBag info, Redirect will lose the bag
+        return View("result");
+    }
+
+    [HttpGet("results")]
+    public ViewResult Result()
+    {
+        return View("result");
+    }
+
+//^^ work in here -----------------------------------------------------------------------------------------//
+    [HttpGet("privacy")]
     public IActionResult Privacy()
     {
         return View();
+    }
+
+    [HttpGet("{**path}")]
+    public RedirectResult Unknown()
+    {
+        return Redirect("/");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- assignments/Dojo_Survey/Program.cs
+++ assignments/Dojo_Survey/Program.cs
@@ -10,10 +10,9 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
